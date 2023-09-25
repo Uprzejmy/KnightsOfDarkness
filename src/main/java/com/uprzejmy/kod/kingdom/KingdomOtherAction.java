e9d3b43e76ea4c6730e7ab68837ff70e7dc221b6
Uprzejmy <lmerynda@gmail.com>
2023-09-25 21:32:41 +0000

    make sure kingdom will not buy land it cannot afford

--- src/main/java/com/uprzejmy/kod/kingdom/KingdomOtherAction.java
+++ src/main/java/com/uprzejmy/kod/kingdom/KingdomOtherAction.java
@@ -27,10 +27,12 @@ public class KingdomOtherAction
      */
     public int buyLand(int count)
     {
+        // TODO tests, check situations where we could potentially go below 0
         // check more sophisticated math like this: https://math.stackexchange.com/questions/296476/formula-for-calculating-the-sum-of-a-series-of-function-results-over-a-specific
 
         // dumb approach to keep adding next calculated cost for each piece of land
         var availableGold = kingdom.getResources().getCount(ResourceName.gold);
+        assert availableGold >= 0;
         var initialLand = kingdom.getResources().getCount(ResourceName.land);
 
         var transaction = calculateCost(initialLand, count, availableGold);
@@ -43,14 +45,17 @@ public class KingdomOtherAction
 
     static LandTransaction calculateCost(int initialLand, int count, int availableGold)
     {
+        // TODO split this function to separately cover cost and how much can be bought for a given amount of gold
         var power = 2; // linear function f(x) = 1/10 * x^2
         var landPrice = 0;
         var landToBuy = 0;
-        while (landToBuy < count && landPrice <= availableGold)
+        var nextCalculatedPrice = (int) Math.round(Math.pow(initialLand + 1, power) / 10);
+        while (landToBuy < count && landPrice + nextCalculatedPrice <= availableGold)
         {
+            landPrice += nextCalculatedPrice;
             landToBuy++;
-            var priceFactor = initialLand + landToBuy;
-            landPrice += Math.pow(priceFactor, power) / 10;
+            var priceFactor = initialLand + landToBuy + 1;
+            nextCalculatedPrice = (int) Math.round(Math.pow(priceFactor, power) / 10);
         }
 
         return new LandTransaction(landToBuy, landPrice);
